31f6b3177ea01fa526a4625cd1aaafa5dae9a79b
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-10-07 06:28:09 +0000

    Deploy show1e/2019-2-TRACK-Frontend-A-Belikov to github.com/show1e/2019-2-TRACK-Frontend-A-Belikov.git:gh-pages

---
+++ precache-manifest.60adc25081c480d398068c968f40b209.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff11d746af2b71cc7ef63ea50352525c",
+    "url": "/your_repo_name/index.html"
+  },
+  {
+    "revision": "790427871e6526066923",
+    "url": "/your_repo_name/static/css/main.b4eae362.chunk.css"
+  },
+  {
+    "revision": "a74f45beb2e8b44717fb",
+    "url": "/your_repo_name/static/js/2.6d838795.chunk.js"
+  },
+  {
+    "revision": "790427871e6526066923",
+    "url": "/your_repo_name/static/js/main.066043be.chunk.js"
+  },
+  {
+    "revision": "9b386b53ffb2387b00dd",
+    "url": "/your_repo_name/static/js/runtime-main.1a300381.js"
+  },
+  {
+    "revision": "795fb555bea801821f51202dc10aaa78",
+    "url": "/your_repo_name/static/media/logo.795fb555.svg"
+  }
+]);
